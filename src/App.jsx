10c6448bca2c1c1a80338721aa92857428f872dd
Jhenifer-Dias <jheniferad.12@gmail.com>
2024-10-08 21:17:57 -0300

    arrow. 8/10

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,108 @@
-import {Box} from '@mui/material'
+import { Box } from '@mui/material'
+import Button from './Button'
+import Buttonleft from './Buttonleft'
 
-function App (){
+function App() {
   return (
     <Box
-    width="100%"
-    height="100vh"
-    bgcolor="purple"
+      width="100%"
+      height="100vh"
+      bgcolor="purple"
+      display="flex"
+      flexDirection="row-reverse"
+      justifyContent="flex-end"
     >
-      <Box
-      width="45%"
-      height="100%"
-      bgcolor="cyan"
-      >
+  
+    
 
-    </Box>
+      <Box
+        width="60%"
+        height="100%"
+        bgcolor="violet">
+        <Box
+          width="100%"
+          height="30%"
+          bgcolor="grey"
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          
+          >
+        <Box
+        width="370px"
+        height="70px"
+        bgcolor="chocolate"
+        borderRadius="5px"
+        border="1px solid black">
+        </Box>
+        </Box>
+        <Box
+          width="100%"
+          height="70%"
+          bgcolor="wheat"
+          display="flex"
+          justifyContent="center"
+          alignItems="center">
+            <Box> <Buttonleft/> </Box>
+
+            <Box
+            width="500px"
+            height="350px"
+            bgcolor="darksalmon"
+
+            >
+              
+            </Box>
+            <Box>
+                <Button/>
+              </Box>
+        </Box>
+      </Box>
+      <Box
+        width="40%"
+        height="100%"
+        bgcolor="cyan"
+        display="flex"
+        justifyContent="flex-start"
+        alignItems="flex-end"
+        flexDirection="column">
+        <Box
+          width="100%"
+          height="30%"
+          bgcolor="pink"
+          display="flex"
+          justifyContent="center"
+          alignItems="flex-end"
+
+
+        >
+          <Box
+            width="190px"
+            height="170px"
+            bgcolor="white"
+            borderRadius="5px "
+            border="1px solid black">
+          </Box>
+
+        </Box>
+
+        <Box
+          width="100%"
+          height="70%"
+          bgcolor="blue"
+          display="flex"
+          justifyContent="center"
+          alignItems="flex-end"
+        >
+          <Box
+            width="80%"
+            height="430px"
+            bgcolor="white"
+            border="1px solid black"
+            borderRadius="5px">
+          </Box>
+        </Box>
+      </Box>
     </Box>
   )
 }
---
+++ src/Buttonleft.jsx
@@ -0,0 +1,124 @@
+import React from "react";
+import styled from "styled-components";
+
+const Buttonleft = () => {
+    return (
+        <StyledWrapper>
+            <ul className="wrapper">
+
+
+
+
+                <li className="icon black">
+                    <span className="tooltip">Left</span>
+                    <span>
+                        <svg
+                            viewBox="0 0 16 16"
+                            className="bi bi-chevron-double-left"
+                            fill="currentColor"
+                            height="16"
+                            width="16"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                d="M8.354 1.646a.5.5 0 0 1 0 .708L2.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"
+                                fillRule="evenodd"
+                            />
+                            <path
+                                d="M12.354 1.646a.5.5 0 0 1 0 .708L6.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"
+                                fillRule="evenodd"
+                            />
+                        </svg>
+                    </span>
+                </li>
+
+
+            </ul>
+        </StyledWrapper>
+    );
+};
+
+const StyledWrapper = styled.div`
+  .wrapper {
+  display: inline-flex;
+  list-style: none;
+  height: 120px;
+  width: 100%;
+  padding-top: 40px;
+  font-family: "Poppins", sans-serif;
+  justify-content: center;
+}
+
+.wrapper .icon {
+  position: relative;
+  background: #fff;
+  border-radius: 50%;
+  margin: 10px;
+  width: 50px;
+  height: 50px;
+  font-size: 18px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  box-shadow: 0 10px 10px rgba(0, 0, 0, 0.1);
+  cursor: pointer;
+  transition: all 0.2s cubic-bezier(0.68, -0.55, 0.265, 1.55);
+}
+
+.wrapper .tooltip {
+  position: absolute;
+  top: 0;
+  font-size: 14px;
+  background: #fff;
+  color: #fff;
+  padding: 5px 8px;
+  border-radius: 5px;
+  box-shadow: 0 10px 10px rgba(0, 0, 0, 0.1);
+  opacity: 0;
+  pointer-events: none;
+  transition: all 0.3s cubic-bezier(0.68, -0.55, 0.265, 1.55);
+}
+
+.wrapper .tooltip::before {
+  position: absolute;
+  content: "";
+  height: 8px;
+  width: 8px;
+  background: #fff;
+  bottom: -3px;
+  left: 50%;
+  transform: translate(-50%) rotate(45deg);
+  transition: all 0.3s cubic-bezier(0.68, -0.55, 0.265, 1.55);
+}
+
+.wrapper .icon:hover .tooltip {
+  top: -45px;
+  opacity: 1;
+  visibility: visible;
+  pointer-events: auto;
+}
+
+.wrapper .icon:hover span,
+.wrapper .icon:hover .tooltip {
+  text-shadow: 0px -1px 0px rgba(0, 0, 0, 0.1);
+}
+
+.wrapper svg {
+  fill: green;
+}
+
+.wrapper span:hover,
+.wrapper svg:hover,
+.wrapper .black:hover,
+.wrapper .black:hover .tooltip,
+.wrapper .black:hover .tooltip::before {
+  background: #222;
+  color: white;
+  fill: white;
+}
+
+
+`;
+
+export default Buttonleft;
